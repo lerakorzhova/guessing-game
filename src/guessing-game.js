5e9da6983f328d35feaa69131a6038078560cd88
lerakorzhova <lerakorzhova98@gmail.com>
2017-01-29 02:37:34 +0300

    test1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arrRange = [];
+        this.expected = this.last = this.first = this.length = 0;
+    }
 
     setRange(min, max) {
-
+        this.length = max - min;
+        for (var i = min; i < this.length; i++) {
+            this.arrRange.push(i);
+        }
+        this.first = 0;
+        this.last = this.arrRange.length - 1;
     }
 
     guess() {
-
+        this.expected = Math.ceil(this.first + (this.last - this.first) / 2);
+        return this.arrRange[this.expected];
     }
 
     lower() {
-
+        this.last = this.expected;
     }
 
     greater() {
-
+        this.first = this.expected;
     }
 }
 
